4694551cd3466e5133d0a5adc95ff9e774829856
Bhim magar <shokokomichan098@gmail.com>
2025-12-28 20:18:39 +0545

    By uusing the setter methods

--- class/PythonEncapsulation.py
+++ class/PythonEncapsulation.py
@@ -10,6 +10,16 @@ class Person:
     
     def get_age(self):
         return self._age
+    
+    def set_age(self,age):
+        if age > 0:
+            self._age = age 
+        else:
+            print("Invalid age sorry ")
+
 
 p1 = Person("vim",23)
+print(p1.get_age())
+
+p1.set_age(30)
 print(p1.get_age())
