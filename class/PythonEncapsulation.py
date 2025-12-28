9cd28e7aaf9a140cec921128d6b1c5176dd932b3
Bhim magar <shokokomichan098@gmail.com>
2025-12-28 20:25:45 +0545

    third commit ('Use encapsulation to protect and validate data:
    ')

--- class/PythonEncapsulation.py
+++ class/PythonEncapsulation.py
@@ -8,9 +8,11 @@ class Person:
         self.name = name
         self._age = age
     
+    # Getter method to access age
     def get_age(self):
         return self._age
     
+    # Setter mthod to set age with validation 
     def set_age(self,age):
         if age > 0:
             self._age = age 
@@ -23,3 +25,30 @@ print(p1.get_age())
 
 p1.set_age(30)
 print(p1.get_age())
+
+
+
+class Student:
+    def __init__(self,name):
+        self.name = name
+        self._grade = 0
+
+    def set_grade(self,grade):
+        if 0 <= grade <= 100:
+            self._grade = grade
+        else:
+            print("Invalid grade. Please enter a grade between 0 and 100.") 
+    
+    def get_grade(self):
+        return self._grade
+
+    def is_passing(self):
+        if self._grade >= 60:
+            print("passed!")
+        else:
+            print("failed!")
+
+student = Student("Vim")
+student.set_grade(85)
+print(student.get_grade())
+print(student.is_passing())
