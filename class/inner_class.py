b54b0be9f79426200094f43538ff51f59cdb5676
Bhim magar <shokokomichan098@gmail.com>
2025-12-30 15:38:14 +0545

    the third commit of the inner class and the practical example

--- class/inner_class.py
+++ class/inner_class.py
@@ -8,13 +8,42 @@ class Outer:
     def __init__(self):
         self.name = 'outer class'
 
-class Inner:
-    def __init__(self):
-        self.name = 'inner class'
+    class Inner:
+        def __init__(self):
+            self.name = 'inner class'
     
-    def display(self):
-        print('this is inner class')
+        def display(self):
+            print('this is inner class')
 
 outer = Outer()
 inner = outer.Inner()
 inner.display()
+
+# Pratical example 
+
+class Car:
+    def __init__(self,brand,model):
+        self.brand = brand
+        self.model = model
+        self.engine = self.Engine()
+    
+    class Engine:
+        def __init__(self):
+            self.status = 'off'
+        def start(self):
+            self.status = 'running'
+            print('engine started')
+        def stop(self):
+            self.staus = 'off'
+            print('engine stopped')
+        
+        def drive(self):
+            if self.status == 'running':
+                print(f"Driving the {self.brand} {self.model}")
+            else:
+                print("Start the engine first!")
+
+car = Car('Toyota','Camry')
+print(car.drive())
+print(car.engine.start())
+print(car.drive())
