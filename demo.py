602916fe0460b44de6d12c35a8131da70ed9abd9
bheem-works <shokokomichan098@gmail.com>
2026-04-10 06:36:44 +0545

    Anagram Code submitting

--- demo.py
+++ demo.py
@@ -5,5 +5,9 @@ def isAnagram(str1,str2):
     str1 = str1.replace(" ","").lower()
     str2 = str2.replace(" ","").lower()
 
-    return sorted(str1) == sorted(str2)
+    if sorted(str1) == sorted(str2):
+        return True
+    else:
+        return False
 
+print(isAnagram('bus','sub'))
